fd6889eb61beeffdeb335caf3d7762a7de1089a9
Lan Jiang <jianglan1350@gmail.com>
2019-07-23 22:26:56 +0200

    sh script only downloads valid excel files

--- download-files.sh
+++ download-files.sh
@@ -6,7 +6,21 @@ input="../datafile-links.txt"
 
 while IFS=$'\t' read -ra line
 do
-	echo "${line[0]}"
-	echo "${line[1]}"
-	wget -O "${line[1]}" "${line[0]}"
+	url="${line[0]}"
+	url_last=${url##*/}
+	if [[ $url_last == *xls ]] || [[ $url_last == *xlsx ]] ;
+	then
+		echo $url_last
+		wget -O "$url_last" "${line[0]}"
+	else
+		name="${line[1]}"
+		if [[ $name == *xls ]] || [[ $name == *xlsx ]] ;
+		then
+			echo $name
+			wget -O "$name" "$url"
+		fi
+	fi
+	#echo "${line[0]}"
+	#echo "${line[1]}"
+	#wget -O "${line[1]}" "${line[0]}"
 done < "$input"
